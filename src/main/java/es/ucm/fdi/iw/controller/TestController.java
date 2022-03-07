a6c432e6e4bf1eca31c804fc7e85e10fe48f5ad2
Juan Carrión <hola@juancrrn.io>
2022-03-07 14:21:15 +0100

    Añadido controlador de tests

--- src/main/java/es/ucm/fdi/iw/controller/TestController.java
+++ src/main/java/es/ucm/fdi/iw/controller/TestController.java
@@ -18,54 +18,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.ucm.fdi.iw.model.Friendship;
 import es.ucm.fdi.iw.model.FriendshipKey;
 import es.ucm.fdi.iw.model.User;
 
-/**
- * Root controller
- * 
- * Non-authenticated requests only.
- * 
- * @author Daniel Marín Irún
- * @author Juan Carrión Molina
- * @author Mohamed Ghanem
- * @author Óscar Caro Navarro
- * @author Óscar Molano Buitrago
- * 
- * @version 0.0.1
- */
 @Controller
-public class RootController {
+@RequestMapping(value = "/test")
+public class TestController {
 
-    @Autowired 
+    @Autowired
     private HttpSession session;
 
 	private static final Logger log = LogManager.getLogger(RootController.class);
+
     @Autowired
     private EntityManager entityManager;
-    
-	/**
-     * Home view
-     */
-    @GetMapping("/")
-    public String lobby(Model model) {
-        return "lobby";
-    }
 
-	/**
-     * Login view
-     */
-    @GetMapping("/login")
-    public String login(Model model) {
-        return "login";
-    }
+    
 
     /**
      * Profile view
      */
-    @GetMapping("/profile")
+    @GetMapping("/insert-friendship")
     @Transactional
     public String profile(HttpServletRequest request, HttpServletResponse response, Model model) {
 
@@ -99,28 +75,5 @@ public class RootController {
         model.addAttribute("amigos", amigos);
         return "profile";
     }
-
-    /**
-     * Levels view
-     */
-    @GetMapping("/levels")
-    public String levels(Model model) {
-        return "levels";
-    }
-
-    /**
-     * Game view
-     */
-    @GetMapping("/game")
-    public String game(Model model){
-        return "game";
-    }
-
-    /**
-     * Rankings view
-     */
-    @GetMapping("/rankings")
-    public String rankings(Model model){
-        return "rankings";
-    }
+    
 }
