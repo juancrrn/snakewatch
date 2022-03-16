62ef9f4fc3b5b62ae8c04ed3d45ea7b4fe78d9aa
asmolano <61146725+asmolano@users.noreply.github.com>
2022-03-16 19:13:25 +0100

    Rankings acabados y arregladas las uniqueconstraint

--- src/main/java/es/ucm/fdi/iw/model/RoomUser.java
+++ src/main/java/es/ucm/fdi/iw/model/RoomUser.java
@@ -22,7 +22,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "user", "room" }))
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "user_id", "room_id" }))
 public class RoomUser{
 
     /**
