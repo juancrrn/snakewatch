27eb8d8161b73a6cdf19e62c0a3d77fcdbe64aa3
Juan Carrion <hola@juancrrn.io>
2022-04-22 17:47:10 +0200

    Renombrado IwApplication a SnakewatchServerApp

--- src/main/java/es/ucm/fdi/iw/SnakewatchServerApp.java
+++ src/main/java/es/ucm/fdi/iw/SnakewatchServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IwApplication {
+public class SnakewatchServerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IwApplication.class, args);
+		SpringApplication.run(SnakewatchServerApp.class, args);
 	}
 
 }
