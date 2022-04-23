523b3d8cb2544eab9099de1ec133967d4fc7b925
DaniUCM <damari01@ucm.es>
2022-04-24 00:56:26 +0200

    Made food reapear in other position when eaten
    Fixes player dying when eating and no more space

--- src/main/resources/static/js/game/entities/food.js
+++ src/main/resources/static/js/game/entities/food.js
@@ -9,13 +9,19 @@ export default class Food extends Phaser.GameObjects.Sprite {
 
   processTick() {
     if (this.eaten) {
-      this.eaten = true;
-      // TODO: randomize position
+      this.pos = this.scene.getEmptyCell();
+      if (this.pos !== null) {
+        this.eaten = false;
+        this.setPosition(this.pos.x * 20 + 10, this.pos.y * 20 + 10);
+        if (!this.active) this.setActive(true).setVisible(true);
+      } else if (this.active) {
+        this.setActive(false).setVisible(false);
+      }
     }
   }
 
   canEat(pos) {
-    if (pos.x === this.pos.x && pos.y === this.pos.y) {
+    if (!this.eaten && pos.x === this.pos.x && pos.y === this.pos.y) {
       this.eaten = true;
       return true;
     }
