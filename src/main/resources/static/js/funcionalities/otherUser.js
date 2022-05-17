51fe3807179496315444d9999654040ed70199e9
DaniUCM <damari01@ucm.es>
2022-05-17 21:19:10 +0200

    Overhaul friendship request logic from non own profile

--- src/main/resources/static/js/funcionalities/otherUser.js
+++ src/main/resources/static/js/funcionalities/otherUser.js
@@ -1,97 +1,82 @@
+const chatBox = document.getElementById("chat-box");
+
+const frLoadingMessage = document.getElementById("friendship-loading");
+const frSendButton = document.getElementById("fr-send");
+const frCancelButton = document.getElementById("fr-cancel");
+const frAcceptButton = document.getElementById("fr-accept");
+const frFinishButton = document.getElementById("fr-finish");
+
+function sendMsg() {
+  go("/user/" + USER_ID + "/msg", 'POST', { message: chatBox.value }).then((d) => {
+    // Do nothing
+  }).catch((e) => {
+    console.log("Error: ", e);
+  })
+  chatBox.value = '';
+}
+
+function hideElement(element) {
+  element.style.display = 'none';
+}
+
+function unhideElement(element) {
+  element.style.display = '';
+}
+
+function updateFriendship(id, action) {
+  go("/friendship/" + action + "/" + id + "/", "POST").then((d) => {
+
+    switch (action) {
+      case "send_req":
+        hideElement(frSendButton);
+        unhideElement(frCancelButton);
+        break;
+      case "cancel_req":
+        hideElement(frCancelButton);
+        unhideElement(frSendButton);
+        break;
+      case "accept_req":
+        hideElement(frAcceptButton);
+        unhideElement(frFinishButton);
+        break;
+      case "finish":
+        hideElement(frFinishButton);
+        unhideElement(frSendButton);
+        break;
+
+      default:
+        break;
+    }
+  }).catch((e) => {
+    console.log("Error: ", e);
+  })
+}
+
+// FIXME: should be a GET request
+// Get friendship status and show corresponding button
 document.addEventListener("DOMContentLoaded", () => {
+  go("/friendship/state/" + USER_ID, 'POST', {}).then((d) => {
+      hideElement(frLoadingMessage);
 
-    // envio de mensajes con AJAX
-    let b = document.getElementById("sendmsg");
-    b.onclick = (e) => {
-      e.preventDefault();
-      go(b.parentNode.action, 'POST', {
-        message: document.getElementById("message").value
-      })
-        .then(d => console.log("happy", d))
-        .catch(e => console.log("sad", e))
-        document.getElementById("message").value = ""
-    }
+      switch (d.result) {
+        case "friends":
+          unhideElement(frFinishButton);
+          break;
+        case "request sent":
+          unhideElement(frCancelButton);
+          break;
+        case "request received":
+          unhideElement(frAcceptButton);
+          break;
+        case "not friends":
+          unhideElement(frSendButton);
+          break;
 
-    let frLoadingMessage = document.getElementById("friendship-loading");
-    let frSendButton = document.getElementById("friendship-send-button");
-    let frCancelButton = document.getElementById("friendship-request-cancel-button");
-    let frAcceptButton = document.getElementById("friendship-request-accept-button");
-    let frFinishButton = document.getElementById("friendship-finish-button");
-
-    function hideAll() {
-      frLoadingMessage.style.display = 'none';
-      frSendButton.style.display = 'none';
-      frCancelButton.style.display = 'none';
-      frAcceptButton.style.display = 'none';
-      frFinishButton.style.display = 'none';
-    }
-
-    // Get friendship status and show corresponding button
-    go(G_FRIENDSHIP_STATE_URL, 'POST', {})
-      .then(d => {
-        hideAll();
-
-        if (d.result === "friends") {
-          frFinishButton.style.display = 'inline-block';
-        }
-        else if (d.result === "request sent") {
-          frCancelButton.style.display = 'inline-block';
-        }
-        else if (d.result === "request received") {
-          frAcceptButton.style.display = 'inline-block';
-        }
-        else {
-          frSendButton.style.display = 'inline-block';
-        }
-      })
-      .catch(e => {
-        hideAll();
-        console.log("Error loading friendship status", e)
-      })
-
-
-    // Accion de frSendButton: envio de solicitudes de amistad
-    frSendButton.onclick = (e) => {
-      e.preventDefault();
-      go(frSendButton.parentNode.action, 'POST', {})
-        .then(d => {
-          hideAll();
-          frCancelButton.style.display = 'inline-block';
-        })
-        .catch(e => console.log("sad", e))
-    }
-
-    // Accion de frCancelButton: cancelar solicitud de amistad enviada
-    frCancelButton.onclick = (e) => {
-      e.preventDefault();
-      go(frCancelButton.parentNode.action, 'POST', {})
-        .then(d => {
-          hideAll();
-          frSendButton.style.display = 'inline-block';
-        })
-        .catch(e => console.log("sad", e))
-    }
-
-    // Accion de frAcceptButton: aceptar solicitud de amistad recibida
-    frAcceptButton.onclick = (e) => {
-      e.preventDefault();
-      go(frAcceptButton.parentNode.action, 'POST', {})
-        .then(d => {
-          hideAll();
-          frFinishButton.style.display = 'inline-block';
-        })
-        .catch(e => console.log("sad", e))
-    }
-
-    // Accion de frFinishButton: eliminar amigo
-    frFinishButton.onclick = (e) => {
-      e.preventDefault();
-      go(frFinishButton.parentNode.action, 'POST', {})
-        .then(d => {
-          hideAll();
-          frSendButton.style.display = 'inline-block';
-        })
-        .catch(e => console.log("sad", e))
-    }
-
-  });
+        default:
+          break;
+      }
+    }).catch((e) => {
+      hideElement(frLoadingMessage);
+      console.log("Error loading friendship status. Error: ", e)
+  })
+})
